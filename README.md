02b77d6c45e9cb1f04c9345d5df49e6e96522f43
Bart Massey <bart@cs.pdx.edu>
2014-02-04 23:02:11 -0800

    Petrie Multiplier calculator

---
+++ README.md
@@ -0,0 +1,9 @@
+# Petrie Multiplier calculator
+
+This Python programlet is a Petrie Multiplier (binomial
+model) calculator. Please see <http://fob.po8.org/node/622>
+for details.
+
+This program is licensed under the GPL version 3 or later.
+Please see the file COPYING in the source
+distribution of this software for license terms.
---
+++ petrie.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# Copyright (c) 2014 Bart Massey
+# [This program is licensed under the GPL version 3 or later.]
+# Please see the file COPYING in the source
+# distribution of this software for license terms.
+
+# Evaluate a Petrie Multiplier model for various
+# conversations.
+
+from sys import argv
+
+N = int(argv[1])
+pm = float(argv[2])   # probability of male in population
+# in binary intra-gender conversation...
+psm = float(argv[3])   # probability male partner makes sexist remark
+psf = float(argv[4])   # probability female partner makes sexist remark
+
+phf11 = pm * psm
+phm11 = (1 - pm) * psf
+
+def ph(ph11, i, n):
+    if i < 0:
+        return 0.0
+    if i > n:
+        return 0.0
+    if i == 0 and n == 1:
+        return 1 - ph11
+    if i == 1 and n == 1:
+        return ph11
+    return ph(ph11, i, n - 1) * (1 - ph11) + ph(ph11, i - 1, n - 1) * ph11
+
+def phm(i, n):
+    return ph(phm11, i, n)
+
+def phf(i, n):
+    return ph(phf11, i, n)
+
+for (name, f) in [("phm", phm), ("phf", phf)]:
+    print(name)
+    for n in range(1, N + 1):
+        for i in range(0, n + 1):
+            print(" " + "{0:5.3f}".format(f(i, n))[1:], end="")
+        print()
+    print()
